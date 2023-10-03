a524691fdfd2fd8d238a828a75c23859231393cf
ZiajaNieLubieCie4 <kelme.majers@gmail.com>
2023-10-03 15:14:00 +0200

    Zadanie3

---
+++ 03.10.2023/zadanie3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cout << "Wprowadxz pierwszy bok trojkaty: ";
+	cin >> a;
+	cout << "Wprowadz drugi bok trojkatu ";
+	cin >> b;
+	cout << "Wprowadz trzeci bok trojkatu: ";
+		cin >> c;
+		if (a == 0 || b == 0 || c == 0)
+			cout >> "Blad , wprowadzono wartosc zero";
+		else
+			if (a + b > c && a + c > b && b + c > a)
+				cout << "Z podanycg dlugosci da zrobic sie trojkat:";
+			else
+				cout << "Z podanych dlugosci nie da sie zrobic sie trojkat";
+
+
+}
