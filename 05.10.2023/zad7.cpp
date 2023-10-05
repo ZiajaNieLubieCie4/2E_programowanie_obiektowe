e1611012c59a3a97f5eaa512e28544bf03608ae0
Zacharz <145669961+ZiajaNieLubieCie4@users.noreply.github.com>
2023-10-05 13:09:59 +0200

    Update zad7.cpp

--- 05.10.2023/zad7.cpp
+++ 05.10.2023/zad7.cpp
@@ -9,12 +9,12 @@ int main() {
    cin >> y;
    cout << "Wprowadz trzecia liczbe: ";
    cin >> z;
-   if(x > y && x > x) 
-       cout << x << " jest najwieksze";
-    else if(y > x && y > z)
-        cout << y << " jest najwieksze";
-    else if(z > y && z > x)
-        cout << z << " jest najwieksze";
+   if(x < y && x < x) 
+       cout << x << " jest najmniejsze";
+    else if(y < x && y < z)
+        cout << y << " jest najmniejsze";
+    else if(z < y && z < x)
+        cout << z << " jest najmniejsze";
     else
         cout << "liczby sa takie same";
  }
