b51d010eb1b5124a00e29540376e25b92c802a86
ZiajaNieLubieCie4 <145669961+ZiajaNieLubieCie4@users.noreply.github.com>
2024-09-18 13:48:43 +0200

    Add files via upload

---
+++ 18,09,2024/zadania.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+namespace szalasniegit
+{
+    class program
+
+    {
+        public static void Main(string[] args)
+        {
+
+            float PI = float.Pi;
+
+            string dane = "JakubZacharz";
+
+            int x = 10;
+            float y = (float)x;
+
+            double a = 10.78493;
+            int b = Convert.ToInt32(a);
+
+            Console.WriteLine("Napisz zdanie");
+            string napis = Console.ReadLine();
+            Console.WriteLine(" Twoje zdanie " + napis);
+
+
+        }
+    }
+}
